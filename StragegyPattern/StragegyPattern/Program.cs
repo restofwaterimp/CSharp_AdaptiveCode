e4bf4dfb41a898fceacc13c3f5bd18cf0aebf2d3
tmkz.it@gmail.com <tmkz.it@gmail.com>
2019-07-20 08:48:12 +0900

    StrategyPatternを追加

---
+++ StragegyPattern/StragegyPattern/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StragegyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    public interface IStrategy
+    {
+        void Execute();
+    }
+
+    public class ConcreteStrategyA : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("ConcreteStrategyA.Ecexute()");
+        }
+    }
+
+    public class ConcreteStrategyB : IStrategy
+    {
+        public void Execute()
+        {
+            Console.WriteLine("ConcreteStrategyB.Ecexute()");
+        }
+    }
+
+    public class Context
+    {
+        public Context()
+        {
+            currentStrategy = strategyA;
+
+        }
+
+        public void DoSomething()
+        {
+            currentStrategy.Execute();
+            currentStrategy = (currentStrategy == strategyA) ? strategyB : strategyA;
+
+        }
+
+        private readonly IStrategy strategyA = new ConcreteStrategyA();
+        private readonly IStrategy strategyB = new ConcreteStrategyB();
+        private IStrategy currentStrategy;
+    }
+
+
+
+
+}
